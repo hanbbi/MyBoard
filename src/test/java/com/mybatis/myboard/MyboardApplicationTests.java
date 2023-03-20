1512f3952ce70211850e60f6b70faae2936538ec
유한비 <119904360+hanbbi@users.noreply.github.com>
2023-03-20 13:56:36 +0900

    first commit

---
+++ src/test/java/com/mybatis/myboard/MyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatis.myboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
